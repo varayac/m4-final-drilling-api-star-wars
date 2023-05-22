116855191662fca094eb4982f239898c0e471e77
Víctor Araya <victor.d.araya@gmail.com>
2023-05-22 12:44:08 -0400

    Proyecto cumple con requerimientos solicitados, se espera agregar animaciones a futuro

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-### Final Drilling Modulo 4
+# Prueba de Consolidacion M4 - Consumo de API Star Wars
+
+## Proyecto 🚀
+
+Este proyecto corresponde a la prueba de consolidacion del modulo 4, para el bootcamp **"Desarrollo de Aplicaciones Full Stack JavaScript Trainee V2.0 Vespertino"** provisionado en la plataforma **E-Camp** y se consolida lo aprendido en dicho modulo.
+
+El proyecto consiste en consumir la API gratuita de [Star Wars (SWAPI)](https://swapi.dev/) y generar un sitio web responsivo que muestre la altura y peso de los personajes en tarjetas mediante 'mouseover'.
+
+![my-app](./assets/img/portfolio/home.png)
+
+## Requisitos (checklist) 📋
+
+1. Deberás consumir la API gratuita de Star Wars. ✅
+2. Al ingresar al enlace entregado deberás evaluar cual es la ruta correspondiente para poder realizar el ejercicio. ✅
+3. Deberás obtener el nombre, Altura y Peso de los personajes. ✅
+4. Debera segmentar los llamados por lotes de 5 personajes ✅
+5. Desplegar informacion de la API en el front mediante cards. ✅
+
+## Técito Time ☕
+
+En este proyecto los elementos fueron renderizados via etiquetas Template de HTML, cloneNode, fragment y appendChild de JS, para conseguir el efecto deseado. Puede encontrar mas información de estos metodos en Recursos.
+
+## Recursos Utilizados 📌
+
+-   [Bootstrapt 5.3](https://getbootstrap.com/)
+-   [AnimatiSS](https://xsgames.co/animatiss/)
+-   [SWAPI](https://swapi.dev/)
+-   [Template](https://developer.mozilla.org/es/docs/Web/HTML/Element/template)
+-   [Fragment](https://developer.mozilla.org/es/docs/Web/API/Document/createDocumentFragment)
+-   [cloneNode](https://developer.mozilla.org/es/docs/Web/API/Node/cloneNode)
+-   [appendChild](https://developer.mozilla.org/es/docs/Web/API/Node/appendChild)
+-   [Guias Bluuweb 👍🏼](https://bluuweb.dev/04-javascript/05-dom.html)
+
+---
+
+⌨️ con ❤️ por [Varayac (Víctor Araya)](https://github.com/varayac) 😊
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,9 @@
 const BASE_URL = 'https://swapi.dev/api/people/'
-let popularGen
-let isDone = false
+// let popularData, secondaryData, significantData
+// let popularGen, secondaryGen, significantGen
+let isPopularDone = false
+let isSecondaryDone = false
+let isSignificantDone = false
 
 const fetchData = async () => {
     /*  
@@ -20,25 +23,38 @@ const fetchData = async () => {
     const [page1, page2] = await Promise.all([responsePage1.json(), responsePage2.json()])
     const allData = [...page1.results, ...page2.results]
 
+    const popularPeople = await allData.slice(0, 5)
+    const secondaryPeople = await allData.slice(5, 10)
+    const significantPeople = await allData.slice(11, 16)
+
     try {
-        console.log('allData: ', allData)
-        return allData
+        console.log('allPeople: ', allData)
+        console.log('popularPeople: ', popularPeople)
+        console.log('secondaryPeople: ', secondaryPeople)
+        console.log('significantPeople: ', significantPeople)
+
+        return { allData, popularPeople, secondaryPeople, significantPeople }
+        // return allData
     } catch (error) {
         console.error('Error al obtener datos de la API: ', error)
         return []
     } finally {
         // loadingData(false)
-        console.log('YA CARGUE...')
+        console.log('DATOS CARGADOS EXITOSAMENTE...')
     }
 }
 
+// loadingData = () => {}
+
+// BLOQUE PRIMEROS 5 (0-5) PERSONAJES
+// Funcion generadora que recorre y pinta los personajes "populares"(?) via template en row1
 function* popularGenerator(data) {
     const popularSection = document.getElementById('popular-section')
     const popularTemplate = document.getElementById('popular-template')
     const fragment = document.createDocumentFragment()
-    const popularData = data.splice(0, 5)
+    // popularData = data.slice(0, 5)
 
-    for (let item of popularData) {
+    for (let item of data) {
         // yield console.log(`Nombre: ${item.name} Altura: ${item.height} Peso: ${item.mass}`)
         const clone = popularTemplate.content.cloneNode(true)
         clone.querySelector('.card-title').textContent = item.name
@@ -48,24 +64,95 @@ function* popularGenerator(data) {
         yield popularSection.appendChild(fragment)
     }
 }
-
-const printPopularCards = () => {
-    if (!isDone) {
+// Funcion que gatilla la funcion generadora y valida su estado
+const triggerPopularCards = (popularGen) => {
+    if (!isPopularDone) {
         const { value, done } = popularGen.next()
-        console.log(`VALUE: ${value}, DONE: ${done}`)
-        isDone = done
+        console.log(`popularGen VALUE: ${value}, DONE: ${done}`)
+        isPopularDone = done
     } else {
         console.log('Nada mas que imprimir')
     }
 }
+// FIN BLOQUE PRIMEROS 5 (0-5) PERSONAJES
+
+// BLOQUE 6-11 (5-10) PERSONAJES
+// Funcion generadora que recorre y pinta los personajes "secundarios"(?) via template en row2
+function* secondaryGenerator(data) {
+    const secondarySection = document.getElementById('secondary-section')
+    const secondaryTemplate = document.getElementById('secondary-template')
+    const fragment = document.createDocumentFragment()
+    // secondaryData = data.slice(5, 11)
+
+    for (let item of data) {
+        // yield console.log(`Nombre: ${item.name} Altura: ${item.height} Peso: ${item.mass}`)
+        const clone = secondaryTemplate.content.cloneNode(true)
+        clone.querySelector('.card-title').textContent = item.name
+        clone.querySelector('.height').textContent = item.height
+        clone.querySelector('.mass').textContent = item.mass
+        fragment.appendChild(clone)
+        yield secondarySection.appendChild(fragment)
+    }
+}
+// Funcion que gatilla la funcion generadora y valida su estado
+const triggerSecondaryCards = (secondaryGen) => {
+    if (!isSecondaryDone) {
+        const { value, done } = secondaryGen.next()
+        console.log(`secondaryGen VALUE: ${value}, DONE: ${done}`)
+        isSecondaryDone = done
+    } else {
+        console.log('Nada mas que imprimir')
+    }
+}
+// FIN BLOQUE 6-11 (5-10) PERSONAJES
+
+// BLOQUE 12-17 (11-16) PERSONAJES
+// Funcion generadora que recorre y pinta los personajes "significativos"(?) via template en row3
+function* significantGenerator(data) {
+    const significantSection = document.getElementById('significant-section')
+    const significantTemplate = document.getElementById('significant-template')
+    const fragment = document.createDocumentFragment()
+    // secondaryData = data.slice(5, 11)
+
+    for (let item of data) {
+        // yield console.log(`Nombre: ${item.name} Altura: ${item.height} Peso: ${item.mass}`)
+        const clone = significantTemplate.content.cloneNode(true)
+        clone.querySelector('.card-title').textContent = item.name
+        clone.querySelector('.height').textContent = item.height
+        clone.querySelector('.mass').textContent = item.mass
+        fragment.appendChild(clone)
+        yield significantSection.appendChild(fragment)
+    }
+}
+// Funcion que gatilla la funcion generadora y valida su estado
+const triggerSignificantCards = (significantGen) => {
+    if (!isSignificantDone) {
+        const { value, done } = significantGen.next()
+        console.log(`significantGen VALUE: ${value}, DONE: ${done}`)
+        isSignificantDone = done
+    } else {
+        console.log('Nada mas que imprimir')
+    }
+}
+// FIN BLOQUE 12-17 (11-16) PERSONAJES
 
 document.addEventListener('DOMContentLoaded', async () => {
     const popularMainCard = document.getElementById('popular-main-card')
     const secondaryMainCard = document.getElementById('secondary-main-card')
     const significantMainCard = document.getElementById('significant-main-card')
+    // Se cargan los arrays cortados de la funcion fetch
+    const { popularPeople, secondaryPeople, significantPeople } = await fetchData()
 
-    const data = await fetchData()
-    popularGen = popularGenerator(data)
+    const popularGen = popularGenerator(popularPeople)
+    const secondaryGen = secondaryGenerator(secondaryPeople)
+    const significantGen = significantGenerator(significantPeople)
 
-    popularMainCard.addEventListener('click', () => printPopularCards())
+    popularMainCard.addEventListener('mouseover', () => triggerPopularCards(popularGen))
+    secondaryMainCard.addEventListener('mouseover', () => triggerSecondaryCards(secondaryGen))
+    significantMainCard.addEventListener('mouseover', () => triggerSignificantCards(significantGen))
+    /* 
+    popularMainCard.addEventListener('click', () => triggerPopularCards(popularGen))
+    secondaryMainCard.addEventListener('click', () => triggerSecondaryCards(secondaryGen))
+    significantMainCard.addEventListener('click', () => triggerSignificantCards(significantGen)) 
+    */
 })
